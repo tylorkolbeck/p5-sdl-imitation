85be027aed415596842db45c83abf829e1f38183
tylorkolbeck <tylor.kolbeck@gmail.com>
2026-01-10 09:19:03 -0500

    add gfx helper library

--- src/engine/Graphics.hpp
+++ src/engine/Graphics.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "SDL3_gfxPrimitives.h"
 #include <SDL3/SDL.h>
 #include <cmath>
 
@@ -23,32 +24,11 @@ public:
 
   // 2d shapes
   void Line(float x1, float y1, float x2, float y2) {
-    SDL_RenderLine(m_Renderer, x1, y1, x2, y2);
+    aalineRGBA(m_Renderer, x1, y1, x2, y2, 255, 255, 0, 255);
   }
 
   void Circle(float centerX, float centerY, float radius) {
-    int sides = (int)(radius * 6.0f); // tweak this
-    if (sides < 32)
-      sides = 32;
-    if (sides > 512)
-      sides = 512;
-    // int sides =
-    //     static_cast<int>((2.0f * PI * radius) / 2.0f); // 1 segment per 2px
-    if (sides < 10)
-      sides = 10;
-
-    float angle_inc = (2.0f * PI) / static_cast<float>(sides);
-    for (int i = 0; i < sides; ++i) {
-      float angle = i * angle_inc;
-      float next_angle = (i + 1) * angle_inc;
-
-      float x1 = centerX + radius * cos(angle);
-      float y1 = centerY + radius * sin(angle);
-      float x2 = centerX + radius * cos(next_angle);
-      float y2 = centerY + radius * sin(next_angle);
-
-      Line(x1, y1, x2, y2);
-    }
+    aacircleRGBA(m_Renderer, centerX, centerY, radius, 255, 255, 0, 255);
   }
 
   void Rect(float x, float y, float width, float height) {
